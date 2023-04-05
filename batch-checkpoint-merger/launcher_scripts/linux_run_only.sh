e8c3cdfdaec000254df0f091aaf56ddbbb528166
Adam Oswald <adamoswald69420@gmail.com>
2023-04-05 12:28:52 -0400

    Delete batch-checkpoint-merger directory

--- batch-checkpoint-merger/launcher_scripts/linux_run_only.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source "./venv/bin/activate"
-nohup python -m batch_checkpoint_merger >/dev/null 2>&1 &
--- batch-checkpoint-merger/launcher_scripts/win_install_update_run.bat
+++
@@ -1,2 +0,0 @@
-venv\Scripts\python.exe -m pip install --upgrade batch_checkpoint_merger
-start venv\Scripts\pythonw.exe -m batch_checkpoint_merger
